78f87c2cc9f2652bae76ed8f486f2d2f91f3c0fc
YouYou94 <ybh4115@gmail.com>
2022-09-28 14:05:43 +0900

    pages. MainPage export

--- src/Pages/AppRouter.jsx
+++ src/Pages/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MainPage } from './MainPage';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* 메인 페이지 */}
-        <Route />
+        <Route path="/order-app" element={<MainPage />} />
         {/* 주문 페이지 */}
         <Route />
       </Routes>
