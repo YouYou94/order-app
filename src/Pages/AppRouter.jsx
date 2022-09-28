6abbbdf7aae752ebf8c42c9c9a9d10dd65467bb2
YouYou94 <ybh4115@gmail.com>
2022-09-28 14:24:45 +0900

    pages. 서버 API 데이터 받아오기 성공!

--- src/Pages/AppRouter.jsx
+++ src/Pages/AppRouter.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MainPage } from './MainPage';
 import { OrderPage } from './OrderPage';
+import axios from 'axios';
 
 export const AppRouter = () => {
+  const [user, setUser] = useState(null);
+
+  const getUser = async () => {
+    await axios
+      .get('http://localhost:4000/users/1n2pgi02k5')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
