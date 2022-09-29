737bc0c47f075136286b4ffd9f870e7002f65fc9
YouYou94 <ybh4115@gmail.com>
2022-09-29 15:03:26 +0900

    OrderPage. 결과 State 생성

--- src/Pages/OrderPage.jsx
+++ src/Pages/OrderPage.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { Orderer } from '../Components/Orderer/Orderer.jsx';
 import * as Styled from './styled.jsx';
 
 export const OrderPage = ({ user }) => {
+  const [result, setResult] = useState({
+    orderer: {
+      address: user.address,
+      address_detail: user.address_detail,
+    },
+  });
+
   return (
     <Styled.PageContainer>
       <Orderer user={user} />
