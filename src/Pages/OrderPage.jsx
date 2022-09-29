97ef64ecdf2957e9e67079ca0293df9ad52b1d75
YouYou94 <ybh4115@gmail.com>
2022-09-29 14:19:46 +0900

    Orderer. OrderPage export

--- src/Pages/OrderPage.jsx
+++ src/Pages/OrderPage.jsx
@@ -1,5 +1,10 @@
+import { Orderer } from '../Components/Orderer/Orderer.jsx';
 import * as Styled from './styled.jsx';
 
 export const OrderPage = () => {
-  return <Styled.PageContainer></Styled.PageContainer>;
+  return (
+    <Styled.PageContainer>
+      <Orderer />
+    </Styled.PageContainer>
+  );
 };
