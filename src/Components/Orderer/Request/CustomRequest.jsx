d2a95e7796b7107f37fe1b3f44dc10d04182dd35
YouYou94 <ybh4115@gmail.com>
2022-09-30 14:20:14 +0900

    CustomRequest. Textarea 변경 기능 함수 구현

--- src/Components/Orderer/Request/CustomRequest.jsx
+++ src/Components/Orderer/Request/CustomRequest.jsx
@@ -4,9 +4,21 @@ import * as Styled from '../styled.jsx';
 export const CustomRequest = ({ setResultRequest }) => {
   const [custom, setCustom] = useState('');
 
+  const onHandlerChangeCustom = event => {
+    const { value } = event.target;
+    setCustom(value);
+    setResultRequest(current => ({
+      ...current,
+      request: value,
+    }));
+  };
+
   return (
     <>
-      <Styled.CustomRequestTextArea value={custom} />
+      <Styled.CustomRequestTextArea
+        value={custom}
+        onChange={onHandlerChangeCustom}
+      />
       <Styled.CustomTextLength>{custom.length}/60</Styled.CustomTextLength>
     </>
   );
