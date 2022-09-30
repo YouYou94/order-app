eaeecae3148d7787be17c37dda5401f792da1b87
YouYou94 <ybh4115@gmail.com>
2022-09-30 16:22:54 +0900

    Discount. DiscountMethod. Point export

--- src/Components/Discount/Discount.jsx
+++ src/Components/Discount/Discount.jsx
@@ -1,4 +1,5 @@
 import { Coupon } from './DiscountMethod/Coupon.jsx';
+import { Point } from './DiscountMethod/Point.jsx';
 import * as Styled from './styled.jsx';
 
 export const Discount = () => {
@@ -6,6 +7,7 @@ export const Discount = () => {
     <Styled.DiscountWrap>
       <Styled.DiscountTitle>할인 수단 선택</Styled.DiscountTitle>
       <Coupon />
+      <Point />
     </Styled.DiscountWrap>
   );
 };
