0fadb328449d57c70a0fe0f355da0bb0f1da6975
YouYou94 <ybh4115@gmail.com>
2022-10-03 13:49:14 +0900

    Discount. DiscountMethod. Coupon. styled-component 수정 및 추가

--- src/Components/Discount/DiscountMethod/Coupon.jsx
+++ src/Components/Discount/DiscountMethod/Coupon.jsx
@@ -1,5 +1,18 @@
 import * as Styled from '../styled.jsx';
 
 export const Coupon = () => {
-  return <Styled.DiscountMethodWrap></Styled.DiscountMethodWrap>;
+  return (
+    <Styled.DiscountMethodWrap>
+      <Styled.CouponWrap>
+        <Styled.DiscountMethodLabel>쿠폰</Styled.DiscountMethodLabel>
+        <Styled.DiscountMethodLabel color="blueviolet" weight="bold">
+          사용 가능한 쿠폰 장
+        </Styled.DiscountMethodLabel>
+      </Styled.CouponWrap>
+      <Styled.CouponWrap>
+        <Styled.CouponInput type="text" />
+        <Styled.CouponBtn>선택</Styled.CouponBtn>
+      </Styled.CouponWrap>
+    </Styled.DiscountMethodWrap>
+  );
 };
--- src/Components/Discount/styled.jsx
+++ src/Components/Discount/styled.jsx
@@ -18,4 +18,50 @@ export const DiscountTitle = styled.label`
   font-weight: bold;
 `;
 
-export const DiscountMethodWrap = styled.div``;
+export const DiscountMethodWrap = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  margin: 1rem 0;
+
+  padding: 0.5rem;
+
+  background-color: rgb(200, 200, 200);
+`;
+
+export const DiscountMethodLabel = styled.label`
+  margin-right: 0.5rem;
+
+  color: ${prop => prop.color || 'black'};
+
+  font-weight: ${prop => prop.weight || ''};
+`;
+
+// Coupon
+export const CouponWrap = styled.div`
+  flex: 1;
+
+  display: flex;
+
+  margin-bottom: 0.1rem;
+`;
+
+export const CouponInput = styled.input`
+  flex: 75%;
+`;
+
+export const CouponBtn = styled.button`
+  flex: 25%;
+
+  border: none;
+
+  margin-left: 0.5rem;
+
+  background-color: blueviolet;
+  color: white;
+
+  cursor: pointer;
+`;
