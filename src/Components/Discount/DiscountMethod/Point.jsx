9f289353c804794a57caf532fe470133eb4b1406
YouYou94 <ybh4115@gmail.com>
2022-10-03 13:52:11 +0900

    Discount. DiscountMethod. Point. styled-component 추가 및 수정

--- src/Components/Discount/DiscountMethod/Point.jsx
+++ src/Components/Discount/DiscountMethod/Point.jsx
@@ -1,5 +1,18 @@
 import * as Styled from '../styled.jsx';
 
 export const Point = () => {
-  return <Styled.DiscountMethodWrap></Styled.DiscountMethodWrap>;
+  return (
+    <Styled.DiscountMethodWrap>
+      <Styled.DiscountMethodContentWrap>
+        <Styled.DiscountMethodLabel>포인트</Styled.DiscountMethodLabel>
+        <Styled.DiscountMethodLabel color="blueviolet" weight="bold">
+          사용 가능한 포인트 점
+        </Styled.DiscountMethodLabel>
+      </Styled.DiscountMethodContentWrap>
+      <Styled.DiscountMethodContentWrap>
+        <Styled.DiscountMethodInput type="text" />
+        <Styled.DiscountMethodBtn>선택</Styled.DiscountMethodBtn>
+      </Styled.DiscountMethodContentWrap>
+    </Styled.DiscountMethodWrap>
+  );
 };
