bb7765fb765e731119eccdbd24130f17bc12a65e
YouYou94 <ybh4115@gmail.com>
2022-10-04 13:45:58 +0900

    PayOrder. 결제 하기 버튼 기능 구현 중...

--- src/Components/PayOrder.jsx
+++ src/Components/PayOrder.jsx
@@ -1,9 +1,15 @@
 import * as Styled from './styled.jsx';
 
 export const PayOrder = () => {
+  const onHandlerPayOrder = () => {
+    alert(`주소: \n상세주소: \n전화번호: \n주문 요청 사항: \n결제 수단: \n할인 수단: 
+        \n총 결제 금액: 원\n결제완료!`);
+  };
   return (
     <Styled.PayOrderWrap>
-      <Styled.PayOrderBtn>결제 하기</Styled.PayOrderBtn>
+      <Styled.PayOrderBtn onClick={onHandlerPayOrder}>
+        결제 하기
+      </Styled.PayOrderBtn>
     </Styled.PayOrderWrap>
   );
 };
