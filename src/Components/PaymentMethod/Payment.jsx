d1edbe916deea715a8edf291992f8af0c6ed735e
YouYou94 <ybh4115@gmail.com>
2022-10-06 15:10:09 +0900

    Carousel. 다음 버튼 이전 버튼 애니메이션 추가

--- src/Components/PaymentMethod/Payment.jsx
+++ src/Components/PaymentMethod/Payment.jsx
@@ -1,19 +1,47 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import * as Styled from './styled.jsx';
 
+const FIRST = 'first';
+const SECOND = 'second';
+
 export const Payment = ({ user, setResultPayment }) => {
+  const [card, setCard] = useState(FIRST);
+  const cardSlide = useRef();
+
+  const onHandlerPrev = event => {
+    if (card === FIRST) {
+    } else if (card === SECOND) {
+      setCard(FIRST);
+      cardSlide.current.style.transition = 'transform 0.4s ease-in-out';
+      cardSlide.current.style.transform = 'translateX(0%)';
+    }
+  };
+
+  const onHandlerNext = event => {
+    if (card === SECOND) {
+    } else if (card === FIRST) {
+      setCard(SECOND);
+      cardSlide.current.style.transition = 'transform 0.4s ease-in-out';
+      cardSlide.current.style.transform = 'translateX(-50%)';
+    }
+  };
+
   return (
     <Styled.PaymentWrap>
       <Styled.PaymentTitle>결제 수단 선택</Styled.PaymentTitle>
       <Styled.CarouselContainer>
-        <Styled.CarouselSlide>
+        <Styled.CarouselSlide ref={cardSlide}>
           <Styled.CarouselContent>1번</Styled.CarouselContent>
           <Styled.CarouselContent>2번</Styled.CarouselContent>
         </Styled.CarouselSlide>
       </Styled.CarouselContainer>
       <Styled.CarouselBtnContainer>
-        <Styled.CarouselPrevBtn></Styled.CarouselPrevBtn>
-        <Styled.CarouselNextBtn></Styled.CarouselNextBtn>
+        <Styled.CarouselPrevBtn
+          onClick={onHandlerPrev}
+        ></Styled.CarouselPrevBtn>
+        <Styled.CarouselNextBtn
+          onClick={onHandlerNext}
+        ></Styled.CarouselNextBtn>
       </Styled.CarouselBtnContainer>
     </Styled.PaymentWrap>
   );
