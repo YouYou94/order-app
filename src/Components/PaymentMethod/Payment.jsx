f636cec663edd7d9225a3499d38bd81566178f73
YouYou94 <ybh4115@gmail.com>
2022-10-06 14:53:54 +0900

    Carousel. 캐러셀 슬라이드 stlyed-components 구현

--- src/Components/PaymentMethod/Payment.jsx
+++ src/Components/PaymentMethod/Payment.jsx
@@ -5,6 +5,16 @@ export const Payment = ({ user, setResultPayment }) => {
   return (
     <Styled.PaymentWrap>
       <Styled.PaymentTitle>결제 수단 선택</Styled.PaymentTitle>
+      <Styled.CarouselContainer>
+        <Styled.CarouselSlide>
+          <Styled.CarouselContent>1번</Styled.CarouselContent>
+          <Styled.CarouselContent>2번</Styled.CarouselContent>
+        </Styled.CarouselSlide>
+      </Styled.CarouselContainer>
+      <Styled.CarouselBtnContainer>
+        <Styled.CarouselPrevBtn></Styled.CarouselPrevBtn>
+        <Styled.CarouselNextBtn></Styled.CarouselNextBtn>
+      </Styled.CarouselBtnContainer>
     </Styled.PaymentWrap>
   );
 };
--- src/Components/PaymentMethod/styled.jsx
+++ src/Components/PaymentMethod/styled.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import next from './assets/next.png';
+import prev from './assets/prev.png';
 
 export const PaymentWrap = styled.div`
   flex: 25%;
@@ -11,7 +13,66 @@ export const PaymentWrap = styled.div`
 
   background-color: white;
 `;
+
 export const PaymentTitle = styled.label`
   font-size: 1.5rem;
   font-weight: bold;
+`;
+
+export const CarouselContainer = styled.div`
+  height: 9rem;
+
+  margin: 0.3rem;
+
+  overflow: hidden;
+
+  position: relative;
+`;
+
+export const CarouselSlide = styled.div`
+  width: 200%;
+  height: 100%;
+
+  display: flex;
+`;
+
+export const CarouselContent = styled.div`
+  width: 100%;
+  height: 100%;
+
+  border-radius: 1rem;
+
+  background-color: rgb(225, 225, 225);
+`;
+
+export const CarouselBtnContainer = styled.div`
+  text-align: center;
+`;
+
+export const CarouselPrevBtn = styled.button`
+  width: 1.5rem;
+  height: 1.5rem;
+
+  margin: 0 0.3rem;
+
+  border: none;
+  border-radius: 50%;
+
+  background-image: url(${prev});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const CarouselNextBtn = styled.button`
+  width: 1.5rem;
+  height: 1.5rem;
+
+  margin: 0 0.3rem;
+
+  border: none;
+  border-radius: 50%;
+
+  background-image: url(${next});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
