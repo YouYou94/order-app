95da1eb8189a781e5526d145191600944cd9de19
YouYou94 <ybh4115@gmail.com>
2022-10-06 12:21:14 +0900

    Payment. 코드 정리

--- src/Components/PaymentMethod/Payment.jsx
+++ src/Components/PaymentMethod/Payment.jsx
@@ -2,40 +2,9 @@ import { useState } from 'react';
 import * as Styled from './styled.jsx';
 
 export const Payment = ({ user, setResultPayment }) => {
-  const [isChecked, setIsChecked] = useState({
-    first: true,
-    second: false,
-  });
-  const onHandlerSelectCard = event => {};
-
-  const cardList = user.payment_methods.map(data => {
-    return (
-      <Styled.PaymentCarouselContent key={data.id} value={data.id}>
-        {data.vendor_name}
-      </Styled.PaymentCarouselContent>
-    );
-  });
-
   return (
     <Styled.PaymentWrap>
       <Styled.PaymentTitle>결제 수단 선택</Styled.PaymentTitle>
-      <Styled.PaymentCarouselSlide>
-        <Styled.PaymentCarouselContentWrap>
-          {cardList}
-        </Styled.PaymentCarouselContentWrap>
-      </Styled.PaymentCarouselSlide>
-      <Styled.PaymentCarouselNaviWrap>
-        <Styled.PaymentCarouselNavi
-          checked={isChecked.first}
-          value="1"
-          onClick={onHandlerSelectCard}
-        />
-        <Styled.PaymentCarouselNavi
-          hecked={isChecked.second}
-          value="2"
-          onClick={onHandlerSelectCard}
-        />
-      </Styled.PaymentCarouselNaviWrap>
     </Styled.PaymentWrap>
   );
 };
