d7480edaf976eb07c54364e0d37614c59cf5529d
YouYou94 <ybh4115@gmail.com>
2022-10-11 15:05:50 +0900

    Validation. Point. 결제 금액 < 포인트 유효성 수정

--- src/Components/Discount/DiscountMethod/Point/PointUsing.jsx
+++ src/Components/Discount/DiscountMethod/Point/PointUsing.jsx
@@ -1,5 +1,5 @@
 import * as Styled from '../../styled.jsx';
-import { POINT, FIXED } from '../../../../Constants.js';
+import { POINT, FIXED, NONE } from '../../../../Constants.js';
 
 export const PoingUsing = ({
   point,
@@ -15,15 +15,22 @@ export const PoingUsing = ({
     if (Number(value) > resultPrice) {
       alert('결제 금액보다 많습니다!');
       setPoint(0);
+      setResultDiscount({
+        method: NONE,
+        type: '',
+        value: 0,
+        name: '',
+        price: 0,
+      });
+    } else {
+      setResultDiscount({
+        method: POINT,
+        type: FIXED,
+        value: Number(value),
+        name: '포인트 사용',
+        price: Number(value),
+      });
     }
-
-    setResultDiscount({
-      method: POINT,
-      type: FIXED,
-      value: Number(value),
-      name: '포인트 사용',
-      price: Number(value),
-    });
   };
 
   return (
