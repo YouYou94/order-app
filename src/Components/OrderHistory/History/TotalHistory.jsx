1944af1059868c270919fa926e0fbbadb9de4618
YouYou94 <ybh4115@gmail.com>
2022-10-18 13:46:54 +0900

    TotalHistory. 키 값 추가

--- src/Components/OrderHistory/History/TotalHistory.jsx
+++ src/Components/OrderHistory/History/TotalHistory.jsx
@@ -3,7 +3,9 @@ import * as Styled from '../styled.jsx';
 export const TotalHistory = ({ TOTALPRICE, discountPrice }) => {
   return (
     <>
-      <Styled.OrderHistoryLabel>{TOTALPRICE.name}</Styled.OrderHistoryLabel>
+      <Styled.OrderHistoryLabel key={TOTALPRICE.id}>
+        {TOTALPRICE.name}
+      </Styled.OrderHistoryLabel>
       <Styled.OrderHistoryLabel>
         {(TOTALPRICE.price - discountPrice).toLocaleString('ko-KR')} 원
       </Styled.OrderHistoryLabel>
