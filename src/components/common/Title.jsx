f7d3ecb233150b25194ee183424ddc81b2ac1121
YouYou94 <ybh4115@gmail.com>
2022-12-22 20:05:08 +0900

    Add: SubTitle 추가

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const SectionTitle = ({ children }) => {
-  return;
+  return <MainTitle>{children}</MainTitle>;
 };
 
-export const Title = styled.h1`
+export const SectionSubTitle = ({ bold, children }) => {
+  return <SubTitle bold={bold}>{children}</SubTitle>;
+};
+
+export const MainTitle = styled.h1`
   width: 382px;
   height: 23px;
 
@@ -13,6 +17,19 @@ export const Title = styled.h1`
   font-weight: 700;
   font-size: 20px;
   line-height: 23px;
+
+  color: #212121;
+`;
+
+export const SubTitle = styled.h2`
+  width: 382px;
+  height: 19px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: ${props => props.bold};
+  font-size: 16px;
+  line-height: 19px;
 
   color: #212121;
 `;
