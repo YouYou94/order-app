6398bcfcda0e72d078ff8ae546c0dcd86e8331a8
YouYou94 <ybh4115@gmail.com>
2022-12-22 18:08:15 +0900

    Add: 공통 컴포넌트 타이틀 추가

---
+++ src/components/common/Title.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const SectionTitle = ({ children }) => {
+  return;
+};
+
+export const Title = styled.h1`
+  width: 382px;
+  height: 23px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 23px;
+
+  color: #212121;
+`;
