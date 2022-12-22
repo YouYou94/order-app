5de688c03dcd1f9d9b4b642d92815b8ef092be7d
YouYou94 <ybh4115@gmail.com>
2022-12-22 17:01:43 +0900

    :fire: 코드 리펙터링 시작!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import { AppRouter } from './Pages/AppRouter';
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
