2574f5337a352d7bb37ce4a3864079ba3c028115
YouYou94 <ybh4115@gmail.com>
2022-12-22 17:37:13 +0900

    Add: OrderPage. 주문 페이지 추가

---
+++ src/pages/OrderPage.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default function OrderPage() {
+  return <OrderPageLayout></OrderPageLayout>;
+}
+
+export const OrderPageLayout = styled.div`
+  max-width: 480px;
+  min-width: 320px;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
