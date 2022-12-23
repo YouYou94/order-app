9c5ea7eb94c80794ba5e361ef157cce1bb7978cb
YouYou94 <ybh4115@gmail.com>
2022-12-23 15:27:57 +0900

    Mod: Payment Styled 수정

--- src/components/Payment/Styled.jsx
+++ src/components/Payment/Styled.jsx
@@ -30,8 +30,39 @@ export const PaymentCardBox = styled.div`
   width: 100%;
   height: 220px;
 
+  display: flex;
+  flex-direction: column;
+
   padding: 16px 32px;
   border-radius: 8px;
 
   background-color: rgb(204, 204, 204);
+`;
+
+export const VendorName = styled.span`
+  height: 19px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #212121;
+`;
+
+export const CardNumBox = styled.div`
+  flex: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #212121;
 `;
