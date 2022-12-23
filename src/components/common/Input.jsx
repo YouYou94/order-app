68144064549814685839298a476de0b1de26e43a
YouYou94 <ybh4115@gmail.com>
2022-12-23 12:33:51 +0900

    Mod: Input 수정

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -8,11 +8,15 @@ export const OrdererInfoInput = styled.input`
   width: 366px;
   height: 19px;
 
+  padding 7px;
+  border: 1px solid rgb(204, 204, 204);
+
   font-family: 'Roboto';
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
   line-height: 19px;
+  outline: none;
 
   color: #212121;
 `;
