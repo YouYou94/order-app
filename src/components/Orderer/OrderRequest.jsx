1f30a0117cc111dac9c68cfb1a30ee15ed4ee35b
YouYou94 <ybh4115@gmail.com>
2022-12-23 12:43:20 +0900

    Feat: Custom 레이아웃 추가 및 Select 변경 기능 추가

--- src/components/Orderer/OrderRequest.jsx
+++ src/components/Orderer/OrderRequest.jsx
@@ -1,8 +1,18 @@
-import { DEFAULT, REQUESTLIST } from '../../Constants.js';
+import { useState } from 'react';
+import { CUSTOM, DEFAULT, REQUESTLIST } from '../../Constants.js';
 import { SubTitle } from '../common/Title.jsx';
 import * as Styled from './Styled.jsx';
 
 export default function OrderRequest() {
+  const [isCustom, setIsCustom] = useState(false);
+
+  const onHandlerChangeSelect = event => {
+    const { value } = event.target;
+
+    if (value === CUSTOM) setIsCustom(true);
+    else setIsCustom(false);
+  };
+
   const requestList = REQUESTLIST.map(option => {
     return option.id === DEFAULT ? (
       <Styled.RequestOption
@@ -23,9 +33,20 @@ export default function OrderRequest() {
   return (
     <Styled.RequestBox>
       <SubTitle bold={700}>주문 요청 사항</SubTitle>
-      <Styled.RequestSelect defaultValue={DEFAULT}>
+      <Styled.RequestSelect
+        defaultValue={DEFAULT}
+        onChange={onHandlerChangeSelect}
+      >
         {requestList}
       </Styled.RequestSelect>
+      {isCustom ? (
+        <>
+          <Styled.CustomArea />
+          <Styled.TextLength>0 / 60</Styled.TextLength>
+        </>
+      ) : (
+        <></>
+      )}
     </Styled.RequestBox>
   );
 }
