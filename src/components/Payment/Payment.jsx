976ed43430ca72748a0454033dfe461de6d2a306
YouYou94 <ybh4115@gmail.com>
2022-12-23 15:28:06 +0900

    Mod: Payment 정보 prop 전달

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -2,11 +2,11 @@ import { SectionTitle } from '../common/Title.jsx';
 import PaymentCarousel from './PaymentCarousel.jsx';
 import * as Styled from './Styled.jsx';
 
-export default function Payment() {
+export default function Payment({ payment }) {
   return (
     <Styled.PaymentBox>
       <SectionTitle>결제 수단 선택</SectionTitle>
-      <PaymentCarousel />
+      <PaymentCarousel payment={payment} />
     </Styled.PaymentBox>
   );
 }
