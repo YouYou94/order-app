555d00b7135da965c3578fd3e8254c161d5934b8
YouYou94 <ybh4115@gmail.com>
2022-12-26 15:17:48 +0900

    Add: Context API를 통한 사용자 데이터 전달

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { createContext, useEffect, useState } from 'react';
 import './App.css';
-import { useAxios } from './components/custom/useAxios';
+import { useAxios } from './components/index.jsx';
+import { DEFAULTUSER } from './Constants';
 import Router from './router/Router';
 
+const USER_URL = '/users/1n2pgi02k5';
+export const UserContext = createContext();
+
 function App() {
-  const { response, error, loading } = useAxios('/users/1n2pgi02k5');
+  // 유저 정보
+  const { data, error, loading } = useAxios(USER_URL);
+  const [user, setUser] = useState(DEFAULTUSER);
+  const [coupon, setCoupon] = useState([]);
+
+  useEffect(() => {
+    if (data) setUser(data);
+  }, []);
+
+  const value = { user, coupon };
 
   return (
-    <div className="App">
-      <Router />
-    </div>
+    <UserContext.Provider value={value}>
+      <div className="App">
+        <Router />
+      </div>
+    </UserContext.Provider>
   );
 }
 
