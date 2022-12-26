359892cd7b5529060b29dbc3f20caed48772cb54
YouYou94 <ybh4115@gmail.com>
2022-12-26 15:17:22 +0900

    refactor: 단순 네이밍 및 리턴 값 변경 코드 흐름에는 변화를 주지 않았음

--- src/components/custom/UseAxios.jsx
+++ src/components/custom/UseAxios.jsx
@@ -4,19 +4,23 @@ import { useEffect, useState } from 'react';
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
 export const useAxios = url => {
-  const [response, setResponse] = useState({});
+  const [data, setData] = useState();
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
 
-  const fetchData = () => {
-    axios
+  const fetchData = async () => {
+    await axios
       .get(url)
-      .then(res => setResponse(res.data))
+      .then(res => setData(res.data))
       .catch(err => setError(err))
-      .finally(() => setLoading(false));
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
-  useEffect(() => fetchData(), []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  return { response, error, loading };
+  return { data, error, loading };
 };
