69b73a36a2eb52eeeed1181d936b6570e4093293
YouYou94 <ybh4115@gmail.com>
2022-12-26 17:13:58 +0900

    주문자 정보: 주소 및 전화번호 컴포넌트 export

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,7 +1,36 @@
 import styled from 'styled-components';
+import { Layout, OrdererAddress, OrdererPhoneNum, Title } from '../components';
+import { useState, useContext } from 'react';
+import { UserContext } from '../App';
 
 export default function OrderPage() {
-  return <OrderPageLayout></OrderPageLayout>;
+  const { user } = useContext(UserContext);
+  const { address, phone_number } = user;
+
+  //console.log(address);
+
+  const [result, setResult] = useState({
+    address: `${address.city} ${address.state} ${address.address_line}`,
+    additional_address: `${address.additional_address}`,
+    phone_number: phone_number.replace(/\-/g, ''),
+  });
+
+  console.log(result);
+
+  const prop = { result, setResult };
+
+  return (
+    <OrderPageLayout>
+      <Layout>
+        <Title>주문자 정보</Title>
+        <OrdererAddress prop={prop} />
+        <OrdererPhoneNum prop={prop} />
+      </Layout>
+      <Layout></Layout>
+      <Layout></Layout>
+      <Layout></Layout>
+    </OrderPageLayout>
+  );
 }
 
 export const OrderPageLayout = styled.div`
