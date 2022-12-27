1e441853cc6fce7a28de4a8a1340519ed4f04b82
YouYou94 <ybh4115@gmail.com>
2022-12-27 16:15:09 +0900

    Feat. 포인트 사용 및 포인트 모두 사용 기능 구현

--- src/components/Discount/Point.jsx
+++ src/components/Discount/Point.jsx
@@ -1,7 +1,58 @@
 import { DiscountLabel, DiscountInput, DiscountButton } from '../index.jsx';
 import styled from 'styled-components';
+import { useContext, useState } from 'react';
+import { UserContext } from '../../App.js';
+import { POINT, FIXED } from '../../Constants.js';
 
 export function Point({ prop }) {
+  const { user } = useContext(UserContext);
+  const { result, setResult } = prop;
+  const [point, setPoint] = useState();
+
+  // 유효성 검사 체크리스트
+  // 숫자만 입력 가능 (v)
+  // 보유 포인트보다 작게 ()
+  const onHandlerChangePoint = event => {
+    const { value } = event.target;
+
+    const check = /^[0-9]+$/;
+
+    if (!check.test(value)) {
+      alert('숫자만 입력 가능합니다.');
+      setPoint(0);
+    } else {
+      setPoint(Number(value));
+
+      setResult({
+        ...result,
+        discount: {
+          method: POINT,
+          type: FIXED,
+          value: point,
+          name: '포인트 사용',
+          price: point,
+        },
+      });
+    }
+  };
+
+  // 유효성 검사 체크리스트
+  // 총 금액보다 작거나 같게 ()
+  const onHandlerClickPointAll = () => {
+    setPoint(user.points);
+
+    setResult({
+      ...result,
+      discount: {
+        method: POINT,
+        type: FIXED,
+        value: point,
+        name: '포인트 사용',
+        price: point,
+      },
+    });
+  };
+
   return (
     <PointBox>
       <LabelBox>
@@ -9,12 +60,14 @@ export function Point({ prop }) {
           포인트
         </DiscountLabel>
         <DiscountLabel bold={700} color={'rgb(150, 18, 231)'}>
-          포인트 보유
+          포인트 {user.points.toLocaleString('ko-KR')}보유
         </DiscountLabel>
       </LabelBox>
       <InputBox>
-        <DiscountInput />
-        <DiscountButton>모두 사용</DiscountButton>
+        <DiscountInput value={point} onHandler={onHandlerChangePoint} />
+        <DiscountButton onHandler={onHandlerClickPointAll}>
+          모두 사용
+        </DiscountButton>
       </InputBox>
     </PointBox>
   );
