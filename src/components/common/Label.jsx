5a7cf01d872119dda30bf79167c6e211026c161a
YouYou94 <ybh4115@gmail.com>
2022-12-27 17:42:08 +0900

    Add: CouponLabel 컴포넌트 생성

--- src/components/common/Label.jsx
+++ src/components/common/Label.jsx
@@ -8,12 +8,47 @@ export const DiscountLabel = ({ bold, color, children }) => {
   );
 };
 
+export const CouponLabel = ({
+  bold,
+  size,
+  lineheight,
+  isFlex,
+  color,
+  children,
+  id,
+}) => {
+  return (
+    <CouponValueLabel
+      bold={bold}
+      size={size}
+      lineheight={lineheight}
+      isFlex={isFlex}
+      color={color}
+      id={id}
+    >
+      {children}
+    </CouponValueLabel>
+  );
+};
+
 export const DiscountMethodLabel = styled.label`
   font-family: 'Roboto';
   font-style: normal;
   font-weight: ${prop => prop.bold};
   font-size: 12px;
   line-height: 14px;
+
+  color: ${prop => prop.color};
+`;
+
+export const CouponValueLabel = styled.label`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: ${prop => prop.bold};
+  font-size: ${prop => prop.size}px;
+  line-height: ${prop => prop.lineheight}px;
+
+  ${prop => (prop.isFlex ? 'display:flex; align-items:flex-end;' : '')}
 
   color: ${prop => prop.color};
 `;
