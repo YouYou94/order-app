e5fea4485a93d8987fc7cf434b8577b1299f1fdd
YouYou94 <ybh4115@gmail.com>
2022-12-27 20:37:25 +0900

    Feat. 결제하기 기능 구현

--- src/components/OrderHistory/Pay.jsx
+++ src/components/OrderHistory/Pay.jsx
@@ -2,9 +2,24 @@ import styled from 'styled-components';
 import { PayButton } from '../index.jsx';
 
 export function Pay({ prop }) {
+  const { result } = prop;
+
+  const onHandlerClickPay = () => {
+    alert(
+      `주소: ${result.address}\n상세주소: ${
+        result.additional_address
+      }\n전화번호: ${result.phone_number}\n주문 요청 사항: ${
+        result.request.user_request
+      }\n결제 수단: ${''}\n할인 수단: ${
+        result.discount.method
+      }\n총 결제 금액: ${(
+        result.resultPrice - result.discount.price
+      ).toLocaleString('ko-KR')} 원\n결제완료!`
+    );
+  };
   return (
     <PayBox>
-      <PayButton>결제하기</PayButton>
+      <PayButton onHandler={onHandlerClickPay}>결제하기</PayButton>
     </PayBox>
   );
 }
